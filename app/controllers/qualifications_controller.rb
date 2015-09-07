7b881f4fbb7c6c498ba0ecb0b47db2d20138dee0
Mark Coleman <m@rkcoleman.co.uk>
2015-09-08 00:28:33 +0100

    Remove unnecessary scaffold cruft
    Dont need actions in the controller, some views and some links from the
    index page.
    Also, don't need to test things that don't exist and aren't required.

--- app/controllers/qualifications_controller.rb
+++ app/controllers/qualifications_controller.rb
@@ -12,55 +12,6 @@ class QualificationsController < ApplicationController
   def show
   end
 
-  # GET /qualifications/new
-  def new
-    @qualification = Qualification.new
-  end
-
-  # GET /qualifications/1/edit
-  def edit
-  end
-
-  # POST /qualifications
-  # POST /qualifications.json
-  def create
-    @qualification = Qualification.new(qualification_params)
-
-    respond_to do |format|
-      if @qualification.save
-        format.html { redirect_to @qualification, notice: 'Qualification was successfully created.' }
-        format.json { render :show, status: :created, location: @qualification }
-      else
-        format.html { render :new }
-        format.json { render json: @qualification.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /qualifications/1
-  # PATCH/PUT /qualifications/1.json
-  def update
-    respond_to do |format|
-      if @qualification.update(qualification_params)
-        format.html { redirect_to @qualification, notice: 'Qualification was successfully updated.' }
-        format.json { render :show, status: :ok, location: @qualification }
-      else
-        format.html { render :edit }
-        format.json { render json: @qualification.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /qualifications/1
-  # DELETE /qualifications/1.json
-  def destroy
-    @qualification.destroy
-    respond_to do |format|
-      format.html { redirect_to qualifications_url, notice: 'Qualification was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_qualification
